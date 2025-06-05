d1ed7b3602f35c213620df73218fe7a339caab44
Anandhakrishnan L <lanandha004@gmail.com>
2025-06-05 22:37:19 +0530

    add

--- .medusa/server/src/index.js
+++ .medusa/server/src/index.js
@@ -1,7 +1,13 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const service_1 = require("./modules/rbac/service");
-exports.default = {
-    services: [service_1.default],
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RBAC_MODULE = void 0;
+const service_1 = __importDefault(require("./service"));
+const utils_1 = require("@medusajs/framework/utils");
+exports.RBAC_MODULE = "rbacModuleService";
+exports.default = (0, utils_1.Module)(exports.RBAC_MODULE, {
+    service: service_1.default,
+});
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
