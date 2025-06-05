5ced70484e937399f02eeb8a7071a50e821e546d
Anandhakrishnan L <lanandha004@gmail.com>
2025-06-05 22:18:01 +0530

    added

--- .medusa/server/src/index.js
+++ .medusa/server/src/index.js
@@ -1,3 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const service_1 = require("./modules/rbac/service");
+exports.default = {
+    services: [service_1.default],
+};
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiJ9
